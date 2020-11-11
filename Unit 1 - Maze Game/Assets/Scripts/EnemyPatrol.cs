8916c809214ed4bc87c702fa8d3f39695bfcc3f1
rileysleight <510479@chrome.escg.ac.uk>
2020-11-11 13:47:01 +0000

    enemy move

---
+++ Unit 1 - Maze Game/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+
+    public bool  MoveRight;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (MoveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed , 0,0);
+            transform.localScale= new Vector2 (-2,2);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed , 0,0);
+            transform.localScale= new Vector2 (2,2);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.CompareTag("turn"))
+        {
+            if (MoveRight)
+            {
+                MoveRight = false;
+            }
+            else
+            {
+                MoveRight = true;
+            }
+        }
+        
+    }
+}
---
+++ Unit 1 - Maze Game/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xVelocity = 0;
+        float yVelocity = 0;
+        float speed = 5; 
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            xVelocity =-speed;
+        }
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+;
+            xVelocity=speed;
+        }
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            yVelocity=speed;
+        }
+
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+            yVelocity=-speed;
+        }
+
+
+        rb.velocity = new Vector2(xVelocity, yVelocity);
+    }
+}
